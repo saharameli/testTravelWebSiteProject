71696a086799f77a2ac11d1da3eb4f3338b10864
asus_ <amelii.sahar@gmail.com>
2024-09-24 18:16:31 -0400

    initial commit

---
+++ resources/constants.py
@@ -0,0 +1,6 @@
+TEST_SITE_URL = "https://demo.guru99.com/test/newtours/index.php"
+MAX_WAIT_INTERVAL = 60
+MAX_PAGE_LENGTH = 5
+LOGIN_SUCCESS_MESSAGE_TEXT = "Login Successfully"
+SIGN_OFF_BTN_TEXT ="SIGN-OFF"
+SIGN_ON_BTN_TEXT = "SIGN-ON"
